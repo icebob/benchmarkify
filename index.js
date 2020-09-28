9c8da801d3632da982bd61146cfef798f969d586
zheli-1 <zl3@pdx.edu>
2020-09-28 10:12:44 -0700

    formatnumber return string and false value correction

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const ora = require('ora');
  */
 function formatNumber(value, decimals = 0, sign = false) {
 	if (value == null)
-		value = 0.0;
+		return "-";
 		
 	let res = Number(value.toFixed(decimals)).toLocaleString();
 	if (sign && value > 0.0)
